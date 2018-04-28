ca5bcdc68f349f0e0fa8fd6a6517660a95dc72ae
jarnethorrez <jarne.thorrez@student.howest.be>
2018-04-28 15:07:22 +0200

    Json loading in fine

---
+++ Serenity3/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  Serenity3
+//
+//  Created by Jarne Thorrez on 28/04/2018.
+//  Copyright © 2018 Jarne Thorrez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        loadJson()
+    }
+    
+    func loadJson() {
+        
+        struct city:Decodable {
+            let naam: String
+            let locatie: String
+            let adres: String
+            let stad: String
+            let over: String
+        }
+        
+        let path = Bundle.main.path(forResource: "cities", ofType: "json")
+        let url = URL(fileURLWithPath: path!)
+        
+        do {
+            let data = try Data(contentsOf: url)
+            let cities = try JSONDecoder().decode([String: city].self, from: data)
+            
+            print(cities["1"]!.naam)
+            
+        } catch {
+            print(error)
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
