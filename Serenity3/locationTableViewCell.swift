8a7b568ad19cdb0c024946bafe402379b6e8d0cd
jarnethorrez <jarne.thorrez@student.howest.be>
2018-04-28 15:52:58 +0200

    added outlets

---
+++ Serenity3/locationTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  locationTableViewCell.swift
+//  Serenity3
+//
+//  Created by Jarne Thorrez on 28/04/2018.
+//  Copyright © 2018 Jarne Thorrez. All rights reserved.
+//
+
+import UIKit
+
+class locationTableViewCell: UITableViewCell {
+    
+    @IBOutlet var locationImage: UIImageView!
+    @IBOutlet var locationName: UILabel!
+}
